e08a394c40f4153f81e5b2432b8886fc14519a7b
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-05-22 14:15:46 +0300

    finish commit

---
+++ src/main/java/com/example/telegrambot/repository/NotificationTaskRepository.java
@@ -0,0 +1,15 @@
+package com.example.telegrambot.repository;
+
+import com.example.telegrambot.entity.NotificationTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+
+    List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
+
+}
